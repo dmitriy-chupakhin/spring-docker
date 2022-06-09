cceb58deb637ca3a4c7898f40d21f87a815a6762
dmitriy-chupakhin <ch.dm.dev@gmail.com>
2022-06-09 18:13:42 +0300

    initial commit

---
+++ src/main/java/me/chdm/springbootwithdockerdemo/SpringBootWithDockerDemoApplication.java
@@ -0,0 +1,13 @@
+package me.chdm.springbootwithdockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithDockerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWithDockerDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/chdm/springbootwithdockerdemo/SpringBootWithDockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.chdm.springbootwithdockerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithDockerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
